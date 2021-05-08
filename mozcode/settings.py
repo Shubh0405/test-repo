552a4cb868045d08fbb65cc5655effc7c0bce85c
Shubh0405 <shubhnaresh.gupta2019@vitstudent.ac.in>
2021-05-08 19:49:23 +0530

    discord field added

--- mozcode/settings.py
+++ mozcode/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-f-vg&yk#!)hnr5c$pbmxss)jj@3pspf@gdu3vhhkrianu-o*y^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- reg/models.py
+++ reg/models.py
@@ -8,6 +8,7 @@ class Registration(models.Model):
     number = models.CharField(max_length=254)
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=254, unique=True)
+    discord = models.CharField(max_length=254,null=True)
 
     def __str__(self):
         return self.name
